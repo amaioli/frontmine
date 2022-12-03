a329fd4133b5e9ef8f8c46a650a72a6bcd1a3f0e
Nicemine <alessandro.maioli@gmail.com>
2022-12-03 11:09:46 +0100

    some improvements

--- utilis.py
+++ utilis.py
@@ -1,8 +1,17 @@
-import requests
+import requests, logging
+
+logger = logging.getLogger(__name__)
 
 def checkToken(address):
     url = f'https://api.gopluslabs.io/api/v1/token_security/56?contract_addresses={address}'
-    response = requests.get(url).json()
+    try:
+        response = requests.get(url).json()
+    except:
+        logger.warning("Connection problem with go+")
+        return False
+    if response['message'] != 'OK':
+        logger.warning("Connection problem with go+: " + response['message'])
+        return False
     data = response['result'][address.lower()]
     if data.get("buy_tax") == "0" and data.get("sell_tax") == "0" and data.get('is_honeypot') == "0" and data.get("is_blacklisted") == '0':
         return True
